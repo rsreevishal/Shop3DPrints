b0c6c9ea8ed0dea821799b1eb536819aa6b4ae0d
rsreevishal <rsreevishal111@gmail.com>
2020-12-12 16:13:30 +0530

    added reg-form.html path

--- api/urls.py
+++ api/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('course-details/<int:course_id>', course_details, name='course-details'),
     path('teach-with-us', standard_view('landing/teach-with-us.html'), name='teach-with-us'),
     path('book-trial', standard_view('landing/book-trial.html'), name='book-trial'),
+    path('reg-form', standard_view('landing/reg-form.html'), name='reg-form'),
     path('student', redirect_view('student-courses'), name='student'),
     path('student/course/<int:course_id>', student_course, name='student-course'),
     path('student/courses', student_courses, name='student-courses'),
