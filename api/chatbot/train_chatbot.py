0f8d8006d76da50d90b5b5729401a719206f2c24
rsreevishal <rsreevishal111@gmail.com>
2021-03-06 12:04:11 +0530

    added chatbot

--- api/chatbot/train_chatbot.py
+++ api/chatbot/train_chatbot.py
@@ -1,20 +1,19 @@
 import nltk
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
 import json
 import pickle
-
 import numpy as np
+import random
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
-import random
+from nltk.stem import WordNetLemmatizer
 
+lemmatizer = WordNetLemmatizer()
 words=[]
 classes = []
 documents = []
 ignore_words = ['?', '!']
-data_file = open('intents.json').read()
+data_file = open('/home/sree/SREEVISHAL/Project/Shop3DPrints/api/chatbot/intents.json').read()
 intents = json.loads(data_file)
 
 
